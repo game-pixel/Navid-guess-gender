6e42dedcf0e58a4c43085f6a1588dcce5775b67c
game-pixel <divancoder10@gmail.com>
2021-12-19 04:00:17 +0000

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,22 @@
+<script>
+    let name = ''
+    let gender = ''
+
+    async function getGender() {
+        fetch('https://api.genderize.io/?name=' + name)
+        .then((response) => response.json())
+        .then((data) => {
+  	gender = data.gender; // Path for gender
+	});
+}
+</script>
+
+<h1>Guess Gender</h1>
+
+<form on:submit|preventDefault={getGender}>
+    <label for="name">Name:</label>
+    <input id="name" bind:value={name} type="text" />
+    <button type="submit">Check</button>
+</form>
+
+<p>Gender: {gender}</p>
